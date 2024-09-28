ffb5e3e9024c5ed4cf5dca78f5e9201a6e9b0582
Phat Nguyen <phatnguyentan0491@gmail.com>
2024-09-28 20:39:00 +0700

    update

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.action.onClicked.addListener(() => {
+    chrome.windows.create({
+      url: "https://motify1.github.io/",
+      type: "popup",
+      width: 450,
+      height: 600
+    });
+  });
